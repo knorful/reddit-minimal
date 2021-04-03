db502a988ad8d29ce6a85cb4859ea374a45e265c
Kris Norful <krisnorful@gmail.com>
2021-04-03 15:44:59 -0500

    add search icon to searchbar

--- src/components/Navbar/Searchbar/Searchbar.js
+++ src/components/Navbar/Searchbar/Searchbar.js
@@ -1,13 +1,25 @@
 import React from "react";
-import { Input } from "@material-ui/core";
+import { Input, InputAdornment, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import classes from "./Searchbar.module.css";
 
 export const Searchbar = () => {
   return (
     <div className={classes.Searchbar}>
       <Input
-        placeholder="Search"
+        placeholder="Search..."
         inputProps={{ "aria-label": "description" }}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              type="submit"
+              className={classes.iconButton}
+              aria-label="search"
+            >
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        }
       />
     </div>
   );
