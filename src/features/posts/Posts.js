c69a77ae44fe3ed2ba9c469e32a4c5c5e4a7cdc5
Kris Norful <krisnorful@gmail.com>
2021-04-03 17:13:21 -0500

    use selector methods in postsSlice to grab posts from state

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadAllPosts } from "./postsSlice";
+import { loadAllPosts, selectAllPosts } from "./postsSlice";
 import { PostListItem } from "../../components/PostListItem/PostListItem";
 
 export const Posts = () => {
   const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
 
+  console.log("posts", posts);
   useEffect(() => {
     dispatch(loadAllPosts());
   }, [dispatch]);
