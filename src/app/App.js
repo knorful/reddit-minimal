5769380c61389437da2f786dc660e4db79834f8d
Kris Norful <krisnorful@gmail.com>
2021-04-03 16:13:02 -0500

    import module css file into App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
 import { Container } from "@material-ui/core";
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <Container maxWidth="xl">
-      <div className="App">
+    <Container>
+      <div className={classes.App}>
         <header className="App-header">
           <Navbar />
         </header>
+        <main>
+          <aside>SubReddits</aside>
+          <div>Posts Will Go Here</div>
+        </main>
       </div>
     </Container>
   );
