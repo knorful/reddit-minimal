1cfdc2d9b59513bed020c5b30bf28d029e247434
Kris Norful <krisnorful@gmail.com>
2021-04-03 14:53:33 -0500

    import Material UI input component

--- src/components/Navbar/Searchbar/Searchbar.js
+++ src/components/Navbar/Searchbar/Searchbar.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Input } from "@material-ui/core";
+import classes from "./Searchbar.module.css";
 
 export const Searchbar = () => {
   return (
-    <div>
+    <div className={classes.Searchbar}>
       <input placeholder="Search" />
     </div>
   );
