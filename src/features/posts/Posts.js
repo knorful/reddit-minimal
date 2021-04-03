3671a310a00f02e5d3a673872ff9a4d46350c06a
Kris Norful <krisnorful@gmail.com>
2021-04-03 17:27:07 -0500

    render PostListItem component in Posts component

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -2,15 +2,23 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadAllPosts, selectAllPosts } from "./postsSlice";
 import { PostListItem } from "../../components/PostListItem/PostListItem";
+import classes from "./Posts.module.css";
 
 export const Posts = () => {
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
 
-  console.log("posts", posts);
+  console.log(posts);
+
   useEffect(() => {
     dispatch(loadAllPosts());
   }, [dispatch]);
 
-  return <h2>Posts will go here</h2>;
+  return (
+    <section className={classes.Posts}>
+      {posts.map((post) => (
+        <PostListItem post={post} />
+      ))}
+    </section>
+  );
 };
