565583090b710e70667242c43304738eea957364
Kris Norful <krisnorful@gmail.com>
2021-04-03 12:42:46 -0500

    import Searchbar into Navbar component and add css file for Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Searchbar } from "./Seachbar/Searchbar.js";
+import classes from "./Navbar.module.css";
+
+export const Navbar = () => {
+  return (
+    <div className={classes.Navbar}>
+      <h2>Logo will go here!</h2>
+      <Searchbar />
+    </div>
+  );
+};
