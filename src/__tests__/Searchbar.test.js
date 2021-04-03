fe3e94f94e89b8ac2dec57ffa8213d8a13a80f24
Kris Norful <krisnorful@gmail.com>
2021-04-03 15:22:45 -0500

    write test to check if Material UI Input component renders and has a placeholder

--- src/__tests__/Searchbar.test.js
+++ src/__tests__/Searchbar.test.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { shallow, render, mount } from "enzyme";
+import { createShallow } from "@material-ui/core/test-utils";
 import { Searchbar } from "../components/Navbar/Searchbar/Searchbar";
 
-describe("<Searchbar /> ", () => {
-  test("contains input field", () => {
-    const wrapper = shallow(<Searchbar />);
-    expect(wrapper.find("input").length).toEqual(1);
+describe("<Searchbar />", () => {
+  let shallow;
+
+  beforeAll(() => {
+    shallow = createShallow();
   });
 
-  test("contains placeholder text", () => {
+  test("renders correctly", () => {
+    const wrapper = shallow(<Searchbar />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  test("renders input element with placeholder", () => {
     const wrapper = shallow(<Searchbar />);
     const placeholder = "Search";
-    expect(wrapper.find("input").props().placeholder).toEqual(placeholder);
+    expect(wrapper.props().children.props.placeholder).toEqual(placeholder);
   });
 });
