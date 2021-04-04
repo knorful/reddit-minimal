2ca45164adc07d82a8a4e5167acee530b8651647
Kris Norful <krisnorful@gmail.com>
2021-04-04 16:26:04 -0500

    create list of skeleton components to render while loading posts

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadAllPosts, selectAllPosts } from "./postsSlice";
+import { loadAllPosts, selectAllPosts, isLoading } from "./postsSlice";
 import { PostListItem } from "../../components/PostListItem/PostListItem";
+import { PostListItemSkeleton } from "../../components/Skeletons/PostListItem";
 import classes from "./Posts.module.css";
 
 export const Posts = () => {
   const dispatch = useDispatch();
   const posts = useSelector(selectAllPosts);
+  const loading = useSelector(isLoading);
 
-  console.log(posts);
+  console.log("testing", posts);
 
   useEffect(() => {
     dispatch(loadAllPosts());
@@ -16,9 +18,17 @@ export const Posts = () => {
 
   return (
     <section className={classes.Posts}>
-      {posts.map((post) => (
-        <PostListItem post={post} />
-      ))}
+      {posts.length === 0
+        ? Array(25)
+            .fill(0)
+            .map((skeleton) => (
+              <div style={{ marginBottom: "20px" }}>
+                <PostListItemSkeleton />
+              </div>
+            ))
+        : posts.map((post, key) => (
+            <PostListItem key={key} post={post} loading={loading} />
+          ))}
     </section>
   );
 };
