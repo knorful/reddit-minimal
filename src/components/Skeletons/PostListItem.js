33177ea22262254568f9c64a630ba6989f939ee7
Kris Norful <krisnorful@gmail.com>
2021-04-04 17:49:24 -0500

    add skeleton for footer content

--- src/components/Skeletons/PostListItem.js
+++ src/components/Skeletons/PostListItem.js
@@ -6,7 +6,12 @@ export const PostListItemSkeleton = () => {
     <div className={classes.PostListItemSkeleton}>
       <div className={classes.headerSkeleton}>
         <Skeleton animation="wave" variant="circle" width={40} height={40} />
-        <Skeleton animation="wave" variant="text" width={260} />
+        <Skeleton
+          style={{ marginLeft: "8px" }}
+          animation="wave"
+          variant="text"
+          width={260}
+        />
       </div>
       <Skeleton
         fitContent
@@ -15,6 +20,13 @@ export const PostListItemSkeleton = () => {
         width={480}
         height={105}
         className={classes.paragraphSkeleton}
+      />
+      <Skeleton
+        style={{ marginTop: "10px" }}
+        animation="wave"
+        variant="text"
+        width={200}
+        height={40}
       />
     </div>
   );
