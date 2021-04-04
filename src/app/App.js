4a72643a4773a7bcc8da03d7c828010efb5ec4ee
Kris Norful <krisnorful@gmail.com>
2021-04-04 16:24:30 -0500

    add class to aside for subreddits

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
 import { Posts } from "../features/posts/Posts";
+// import { Subreddits } from "../features/subreddits/Subreddits";
 import { Container } from "@material-ui/core";
 import classes from "./App.module.css";
 
@@ -12,7 +13,7 @@ function App() {
       </header>
       <Container>
         <main>
-          <aside>SubReddits</aside>
+          <aside className={classes.subreddits}>SubReddits</aside>
           <Container maxWidth="md">
             <Posts />
           </Container>
