d1ac99053a292593bfdd88ca0c8ee7a73491dac7
Kris Norful <krisnorful@gmail.com>
2021-04-04 16:58:08 -0500

    make paragraph skeleton responsive to smaller screens

--- src/components/Skeletons/PostListItem.js
+++ src/components/Skeletons/PostListItem.js
@@ -1,14 +1,24 @@
 import { Skeleton } from "@material-ui/lab";
 import classes from "./PostListItem.module.css";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 export const PostListItemSkeleton = () => {
+  const matches = useMediaQuery("(min-width:600px)");
+
   return (
     <div className={classes.PostListItemSkeleton}>
       <div className={classes.headerSkeleton}>
         <Skeleton animation="wave" variant="circle" width={40} height={40} />
         <Skeleton animation="wave" variant="text" width={260} />
       </div>
-      <Skeleton animation="wave" variant="rect" width={580} height={105} />
+      <Skeleton
+        fitContent
+        animation="wave"
+        variant="rect"
+        width={480}
+        height={105}
+        className={classes.paragraphSkeleton}
+      />
     </div>
   );
 };
