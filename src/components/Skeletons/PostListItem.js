d5bdeed637249e4786db1b6c5945cd31cab755d0
Kris Norful <krisnorful@gmail.com>
2021-04-04 16:33:39 -0500

    add wave animation to skeletons

--- src/components/Skeletons/PostListItem.js
+++ src/components/Skeletons/PostListItem.js
@@ -4,9 +4,11 @@ import classes from "./PostListItem.module.css";
 export const PostListItemSkeleton = () => {
   return (
     <div className={classes.PostListItemSkeleton}>
-      <Skeleton variant="text" />
-      <Skeleton variant="circle" width={40} height={40} />
-      <Skeleton variant="rect" width={210} height={118} />
+      <div className={classes.headerSkeleton}>
+        <Skeleton animation="wave" variant="circle" width={40} height={40} />
+        <Skeleton animation="wave" variant="text" width={260} />
+      </div>
+      <Skeleton animation="wave" variant="rect" width={580} height={105} />
     </div>
   );
 };
