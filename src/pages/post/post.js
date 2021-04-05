0791770540f2953163a1a9acddab77a57b946eb0
Kris Norful <krisnorful@gmail.com>
2021-04-05 16:01:40 -0500

    render post comments

--- src/pages/post/post.js
+++ src/pages/post/post.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  loadCommentsForPostId,
+  selectComments,
+} from "../../features/comments/commentsSlice";
 
 export const Post = () => {
-  return <h1>This is the post page!</h1>;
+  let { reddit, id } = useParams();
+  const dispatch = useDispatch();
+  const comments = useSelector(selectComments);
+
+  useEffect(() => {
+    dispatch(loadCommentsForPostId({ reddit, id }));
+  }, [dispatch, reddit, id]);
+
+  return (
+    <div>
+      <h1>Post will go here</h1>
+      <h2>Comments</h2>
+      {comments.map((comment) => (
+        <div>{comment.data.body}</div>
+      ))}
+    </div>
+  );
 };
