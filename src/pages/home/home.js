576cb3e7d7d899d51ee81bff1c1865cf8e518725
Kris Norful <krisnorful@gmail.com>
2021-04-05 13:39:13 -0500

    create pages folder to hold various pages to navigate with routing

---
+++ src/pages/home/home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { Posts } from "../../features/posts/Posts";
+import { Subreddits } from "../../features/subreddits/Subreddits";
+import { Container } from "@material-ui/core";
+import classes from "./home.module.css";
+
+export const Home = () => {
+  return (
+    <div className={classes.App}>
+      <header className="App-header">
+        <Navbar />
+      </header>
+      <Container>
+        <main>
+          <aside className={classes.subreddits}>
+            <Subreddits />
+          </aside>
+          <Container maxWidth="md">
+            <Posts />
+          </Container>
+        </main>
+      </Container>
+    </div>
+  );
+};
