c320b5a9ad81441de857808def304c116e0a82c8
Kris Norful <krisnorful@gmail.com>
2021-04-05 11:17:30 -0500

    add subreddits reducer to store state

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { postsSlice } from "../features/posts/postsSlice";
+import { subredditsSlice } from "../features/subreddits/subredditsSlice";
 
 export default configureStore({
   reducer: {
     posts: postsSlice.reducer,
+    subreddits: subredditsSlice.reducer,
   },
 });
