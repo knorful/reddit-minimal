efaff3f459b72984301c6134617ba7a79979a3d2
Kris Norful <krisnorful@gmail.com>
2021-04-05 11:18:21 -0500

    replace old search bar with material ui search bar

--- src/components/Navbar/Searchbar/Searchbar.js
+++ src/components/Navbar/Searchbar/Searchbar.js
@@ -1,26 +1,11 @@
 import React from "react";
-import { Input, InputAdornment, IconButton } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
+import SearchBar from "material-ui-search-bar";
 import classes from "./Searchbar.module.css";
 
 export const Searchbar = () => {
   return (
     <div className={classes.Searchbar}>
-      <Input
-        placeholder="Search..."
-        inputProps={{ "aria-label": "description" }}
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              type="submit"
-              className={classes.iconButton}
-              aria-label="search"
-            >
-              <SearchIcon />
-            </IconButton>
-          </InputAdornment>
-        }
-      />
+      <SearchBar style={{ backgroundColor: "#f7f7f7" }} />
     </div>
   );
 };
