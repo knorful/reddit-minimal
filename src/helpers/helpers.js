e9952c0e4f6d478512a56e42eb02cda7ff32c466
Kris Norful <krisnorful@gmail.com>
2021-04-05 11:47:37 -0500

    add ampersandConverter to helpers file

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -16,4 +16,10 @@ export const Helpers = {
       ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
       : Math.sign(num) * Math.abs(num);
   },
+
+  ampersandConverter(url) {
+    let regex = /(&amp;|&)/gi;
+    let convertedLink = url ? url.replace(regex, "&") : null;
+    return convertedLink;
+  },
 };
