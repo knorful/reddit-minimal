3f0f701343f2c358b25c49c94601cb1a7c08e153
Kris Norful <krisnorful@gmail.com>
2021-04-05 16:02:34 -0500

    pull useful parts of response from API request for comments

--- src/util/API.js
+++ src/util/API.js
@@ -2,16 +2,23 @@ import axios from "axios";
 
 export const API = {
   async loadSubreddits() {
-    const data = await axios
+    const reddits = await axios
       .get(`http://www.reddit.com/subreddits.json`)
       .then((res) => res.data.data.children);
-    return data;
+    return reddits;
   },
 
   async loadPopularPosts() {
-    const data = await axios
+    const popularPosts = await axios
       .get("https://www.reddit.com/r/popular.json")
       .then((res) => res.data.data.children);
-    return data;
+    return popularPosts;
+  },
+
+  async loadComments(reddit, id) {
+    const comments = await axios
+      .get(`http://www.reddit.com/r/${reddit}/comments/${id}.json`)
+      .then((res) => res.data[1].data.children);
+    return comments;
   },
 };
