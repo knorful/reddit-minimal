cbedc39fc12513cd8c30a6ccc9add116f3681345
Kris Norful <krisnorful@gmail.com>
2021-04-05 14:40:47 -0500

    add route for post page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
+import { PostAdd } from "@material-ui/icons";
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import { Home } from "./pages/home/home";
+import { Post } from "./pages/post/post";
 
 const appRoutes = [
   {
     path: "/",
     page: Home,
+  },
+  {
+    path: "/post/:reddit/comments/:subredditId",
+    page: Post,
   },
 ];
 
