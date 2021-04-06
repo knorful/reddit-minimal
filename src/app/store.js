67315f3f5aa12724700f8bda156fb723230ff41f
Kris Norful <krisnorful@gmail.com>
2021-04-06 12:56:45 -0500

    add subreddit slice to redux store state

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import { commentsSlice } from "../features/comments/commentsSlice";
 import { postsSlice } from "../features/posts/postsSlice";
 import { subredditsSlice } from "../features/subreddits/subredditsSlice";
 import { postSlice } from "../features/post/postSlice";
+import { subredditSlice } from "../features/subreddit/subredditSlice";
 
 export default configureStore({
   reducer: {
     post: postSlice.reducer,
     posts: postsSlice.reducer,
     subreddits: subredditsSlice.reducer,
+    subreddit: subredditSlice.reducer,
     comments: commentsSlice.reducer,
   },
 });
