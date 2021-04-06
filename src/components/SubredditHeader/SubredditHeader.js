67a8d91188b2503464997e95b47662a3807a7c2e
Kris Norful <krisnorful@gmail.com>
2021-04-06 16:51:28 -0500

    add default background if no image available

--- src/components/SubredditHeader/SubredditHeader.js
+++ src/components/SubredditHeader/SubredditHeader.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { Avatar, Container } from "@material-ui/core";
 import classes from "./SubredditHeader.module.css";
 
-export const SubredditHeader = ({ img, backColor }) => {
+export const SubredditHeader = ({ img, backColor, title, icon, reddit }) => {
   return (
-    <div
-      className={classes.SubredditHeader}
-      style={{ backgroundColor: `${backColor}` }}
-    >
-      <img src={img} />
+    <div>
+      <figure
+        className={classes.imgContainer}
+        style={{
+          backgroundColor: backColor ? `${backColor}` : "rgb(65, 64, 64)",
+        }}
+      >
+        {img ? (
+          <img src={img} alt={title} />
+        ) : (
+          <div className={classes.noImg}></div>
+        )}
+      </figure>
+      <div className={classes.headerDivide}>
+        <Container maxWidth="sm">
+          <div className={classes.headerContent}>
+            <Avatar className={classes.avatar} src={icon} alt={title} />
+            <div className={classes.reddit}>
+              <p className={classes.title}>{title}</p>
+              <p className={classes.redditTopic}>{`r/${reddit}`}</p>
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
   );
 };
