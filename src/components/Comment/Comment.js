e8402a5172c693f2557f377b5a23e06e92e2c51c
Kris Norful <krisnorful@gmail.com>
2021-04-05 21:33:20 -0500

    display time in user friendly way

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,6 +1,25 @@
 import React from "react";
 import classes from "./Comment.module.css";
+import { format } from "timeago.js";
+import { Helpers } from "../../helpers/helpers";
+import PublishIcon from "@material-ui/icons/Publish";
 
 export const Comment = ({ comment }) => {
-  return <div className={classes.Comment}>{comment}</div>;
+  const commentAuthor = comment.author;
+  const timeCreated = format(comment.created_utc * 1000);
+  const upvotes = comment.ups;
+
+  return (
+    <div className={classes.Comment}>
+      <div className={classes.header}>
+        <p className={classes.author}>{commentAuthor}</p>
+        <p className={classes.timeCreated}>{timeCreated}</p>
+      </div>
+      <p>{comment.body}</p>
+      <div className={classes.footer}>
+        <PublishIcon />
+        <p className={classes.upvotes}>{Helpers.kFormatter(upvotes)} UPVOTES</p>
+      </div>
+    </div>
+  );
 };
