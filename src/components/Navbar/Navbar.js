562df03fe091506ba4c7922a320862bba7fe02cd
Kris Norful <krisnorful@gmail.com>
2021-04-06 08:11:23 -0500

    remove pattern from background

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Searchbar } from "./Searchbar/Searchbar.js";
 import { Container } from "@material-ui/core";
+import RedditIcon from "@material-ui/icons/Reddit";
 import classes from "./Navbar.module.css";
 
 export const Navbar = () => {
@@ -10,6 +11,7 @@ export const Navbar = () => {
       <Container>
         <div className={classes.NavbarContent}>
           <Link className={classes.logo} to="/">
+            <RedditIcon className={classes.redditLogo} />
             <h2>
               Reddit <span>Minimal</span>
             </h2>
