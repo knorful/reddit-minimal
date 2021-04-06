a1fb56ce13ab6951afb3f85136aa54dddace83c8
Kris Norful <krisnorful@gmail.com>
2021-04-06 13:06:55 -0500

    dispatch method to go fetch subreddits by category selected

--- src/pages/subreddit/subreddit.js
+++ src/pages/subreddit/subreddit.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectSubredditPosts,
+  loadPostsBySubreddit,
+} from "../../features/subreddit/subredditSlice";
 
 export const Subreddit = () => {
+  const dispatch = useDispatch();
+  const subredditPosts = useSelector(selectSubredditPosts);
+  const { reddit } = useParams();
+
+  useEffect(() => {
+    dispatch(loadPostsBySubreddit(reddit));
+  }, [dispatch, reddit]);
+
+  console.log("subredditPosts", subredditPosts);
+
   return (
     <div>
       <h1>Subreddit</h1>
