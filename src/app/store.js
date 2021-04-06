ad849a2a2ce3ae9c9096cbb98ad9da8228aa1d21
Kris Norful <krisnorful@gmail.com>
2021-04-05 20:43:26 -0500

    add post reducer to redux store state

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import { commentsSlice } from "../features/comments/commentsSlice";
 import { postsSlice } from "../features/posts/postsSlice";
 import { subredditsSlice } from "../features/subreddits/subredditsSlice";
+import { postSlice } from "../features/post/postSlice";
 
 export default configureStore({
   reducer: {
+    post: postSlice.reducer,
     posts: postsSlice.reducer,
     subreddits: subredditsSlice.reducer,
     comments: commentsSlice.reducer,
