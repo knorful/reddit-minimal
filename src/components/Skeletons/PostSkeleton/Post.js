f36c550ac29f751e57da64937435a96b823b095e
Kris Norful <krisnorful@gmail.com>
2021-04-05 20:49:57 -0500

    create skeleton for Post page

---
+++ src/components/Skeletons/PostSkeleton/Post.js
@@ -0,0 +1,29 @@
+import { Skeleton } from "@material-ui/lab";
+import classes from "./Post.module.css";
+
+export const PostSkeleton = () => {
+  return (
+    <div className={classes.PostSkeleton}>
+      <Skeleton
+        style={{ marginLeft: "8px" }}
+        animation="wave"
+        variant="text"
+        width={800}
+        height={55}
+      />
+      <Skeleton
+        style={{ marginLeft: "8px" }}
+        animation="wave"
+        variant="text"
+        width={800}
+        height={55}
+      />
+      <Skeleton
+        style={{ marginLeft: "8px" }}
+        animation="wave"
+        variant="rect"
+        height={800}
+      />
+    </div>
+  );
+};
