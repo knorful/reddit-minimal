6f6d4ab0b7eb92ce7c76976b4e005d49b958677b
Kris Norful <krisnorful@gmail.com>
2021-04-06 11:53:56 -0500

    add Link to navigate to selected subreddit page

--- src/components/Subreddit/Subreddit.js
+++ src/components/Subreddit/Subreddit.js
@@ -1,29 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Avatar } from "@material-ui/core";
 import { Helpers } from "../../helpers/helpers";
 import classes from "./Subreddit.module.css";
 
 export const Subreddit = ({ subreddit }) => {
   const subredditTopic = subreddit.data.display_name_prefixed;
+  const subs = Helpers.kFormatter(subreddit.data.subscribers);
   const hasIcon =
     subreddit.data.community_icon.length !== 0 || subreddit.data.icon_img;
+
   const iconLink = hasIcon
     ? Helpers.ampersandConverter(subreddit.data.icon_img)
     : null;
-  const subs = Helpers.kFormatter(subreddit.data.subscribers);
+
   const displayIcon = hasIcon ? (
-    <Avatar src={iconLink} />
+    <Avatar className={classes.icon} src={iconLink} />
   ) : (
     <Avatar>{subreddit.data.display_name_prefixed.substring(2, 3)}</Avatar>
   );
 
   return (
-    <div className={classes.Subreddit}>
-      {displayIcon}
-      <div className={classes.subInfo}>
-        <p className={classes.topic}>{subredditTopic}</p>
-        <p className={classes.subs}>{subs} subscribers</p>
+    <Link to="/subreddit/asdfasd">
+      <div className={classes.Subreddit}>
+        {displayIcon}
+        <div className={classes.subInfo}>
+          <p className={classes.topic}>{subredditTopic}</p>
+          <p className={classes.subs}>{subs} subscribers</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
