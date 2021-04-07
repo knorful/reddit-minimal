76af943117802070d1fde0d405e9fa4a7f287354
Kris Norful <krisnorful@gmail.com>
2021-04-06 20:50:24 -0500

    setup state for user

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API } from "../../util/API";
+
+export const loadUserData = createAsyncThunk("user/loadUserData", (user) =>
+  API.getUserData(user)
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    userData: [],
+    loadingUserData: false,
+    hasErrors: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadUserData.pending, (state, action) => {
+        state.loadingUserData = false;
+        state.hasErrors = false;
+      })
+      .addCase(loadUserData.fulfilled, (state, action) => {
+        state.loadingUserData = false;
+        state.hasErrors = false;
+      })
+      .addCase(loadUserData.rejected, (state, action) => {
+        state.loadingUserData = false;
+        state.hasErrors = false;
+      });
+  },
+});
+
+export default userSlice.reducer;
+export const selectUserData = (state) => state.user.userData;
+export const isLoadingUserData = (state) => state.user.loadUserData;
