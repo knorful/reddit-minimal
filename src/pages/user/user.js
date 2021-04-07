322331f8783c5b0a2633cf433f9e7426af319943
Kris Norful <krisnorful@gmail.com>
2021-04-06 22:02:39 -0500

    dispatch method to fetch user data

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { loadUserData, selectUserData } from "../../features/user/userSlice";
 
 export const User = () => {
+  const { user } = useParams();
+  const dispatch = useDispatch();
+  const selectedUserData = useSelector(selectUserData);
+
+  useEffect(() => {
+    dispatch(loadUserData(user));
+  }, [dispatch, user]);
+
+  console.log("user data", selectedUserData);
+
   return (
     <div>
       <h1>User Page</h1>
