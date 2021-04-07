b3cf1e82d65fe4a13bc149c3e12759b2797a7bb1
Kris Norful <krisnorful@gmail.com>
2021-04-06 20:51:06 -0500

    add user state to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import { postsSlice } from "../features/posts/postsSlice";
 import { subredditsSlice } from "../features/subreddits/subredditsSlice";
 import { postSlice } from "../features/post/postSlice";
 import { subredditSlice } from "../features/subreddit/subredditSlice";
+import { userSlice } from "../features/user/userSlice";
 
 export default configureStore({
   reducer: {
@@ -12,5 +13,6 @@ export default configureStore({
     subreddits: subredditsSlice.reducer,
     subreddit: subredditSlice.reducer,
     comments: commentsSlice.reducer,
+    user: userSlice.reducer,
   },
 });
