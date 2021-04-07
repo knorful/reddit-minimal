9e5a3892405a11259efd94f86ea1b5181027e373
Kris Norful <krisnorful@gmail.com>
2021-04-07 13:47:06 -0500

    destructure props sent in from user page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,17 @@
 import React from "react";
+import classes from "./About.module.css";
 
-export const About = () => {
+export const About = ({ name, cake_day, karma, userImg, bannerImg }) => {
+  console.log("userImg", userImg);
   return (
-    <div>
-      <h1>About</h1>
+    <div className={classes.About}>
+      <div className={classes.header}>
+        {bannerImg ? <img src={bannerImg} alt={`${name} banner`} /> : null}
+        <img src={userImg} alt={`${name} profile pic`} />
+      </div>
+      <p>{name}</p>
+      <p>{karma}</p>
+      <p>{cake_day}</p>
     </div>
   );
 };
