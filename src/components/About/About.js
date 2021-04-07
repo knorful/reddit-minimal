e782f8ea59b17fef37d2ca671b8729630ef0dbd5
Kris Norful <krisnorful@gmail.com>
2021-04-07 16:01:26 -0500

    render karma and cake day only if props for those are passed in

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Helpers } from "../../helpers/helpers";
+import { ReactMarkdown } from "react-markdown";
 import classes from "./About.module.css";
 
-export const About = ({ name, cake_day, karma, userImg, bannerImg, title }) => {
+export const About = ({
+  name,
+  cake_day,
+  karma,
+  userImg,
+  bannerImg,
+  title,
+  created,
+  subCount,
+  active,
+}) => {
   let creationDay = Helpers.getDate(cake_day);
 
   return (
@@ -20,26 +31,47 @@ export const About = ({ name, cake_day, karma, userImg, bannerImg, title }) => {
             style={{ height: "100px", backgroundColor: "#FF4500" }}
           ></div>
         )}
-        <img
-          className={classes.profilePic}
-          src={userImg}
-          alt={`${name} profile pic`}
-        />
+        {userImg ? (
+          <img
+            className={classes.profilePic}
+            src={userImg}
+            alt={`${name} profile pic`}
+          />
+        ) : null}
       </div>
-      <p className={classes.aboutHeader}>About ${name}</p>
+      <p className={classes.aboutHeader}>About {name}</p>
       <hr />
       <div className={classes.info}>
-        <p className={classes.name}>{name}</p>
+        {/* <p className={classes.name}>{name}</p> */}
         <p>{title}</p>
         <div className={classes.additionalInfo}>
-          <div className={classes.karma}>
-            <h3>Karma</h3>
-            <p>{Helpers.kFormatter(karma)}</p>
-          </div>
-          <div className={classes.cake_day}>
-            <h3>Cake Day</h3>
-            <p>{`${creationDay.month} ${creationDay.day}, ${creationDay.year}`}</p>
-          </div>
+          {karma && cake_day ? (
+            <>
+              <div className={classes.karma}>
+                <h3>Karma</h3>
+                <p>{Helpers.kFormatter(karma)}</p>
+              </div>
+              <div className={classes.cake_day}>
+                <h3>Cake Day</h3>
+                <p>{`${creationDay.month} ${creationDay.day}, ${creationDay.year}`}</p>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className={classes.karma}>
+                <h3>Members</h3>
+                <p>{Helpers.kFormatter(subCount)}</p>
+              </div>
+              <div className={classes.cake_day}>
+                <h3>Online</h3>
+                <p>{Helpers.kFormatter(active)}</p>
+              </div>
+              {/* <div className={classes.cake_day}>
+                <h3>Cake Day</h3>
+                <p>{`${creationDay.month} ${creationDay.day}, ${creationDay.year}`}</p>
+              </div> */}
+            </>
+          )}
         </div>
       </div>
     </div>
