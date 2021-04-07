2372bd225e907d05da4d9bc4096a3ca4c7032dee
Kris Norful <krisnorful@gmail.com>
2021-04-07 11:31:50 -0500

    add user show page

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -3,6 +3,10 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { loadUserData, selectUserData } from "../../features/user/userSlice";
 import { PostListItem } from "../../components/PostListItem/PostListItem";
+import { Subreddits } from "../../features/subreddits/Subreddits";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { Container } from "@material-ui/core";
+import classes from "./user.module.css";
 
 export const User = () => {
   const { user } = useParams();
@@ -19,11 +23,23 @@ export const User = () => {
 
   return (
     <div>
-      {approvedUserData ? (
-        approvedUserData.map((userData) => <PostListItem post={userData} />)
-      ) : (
-        <h1>Not yet!!</h1>
-      )}
+      <Navbar />
+      <Container>
+        <main className={classes.User}>
+          <aside>
+            <Subreddits />
+          </aside>
+          <section>
+            {approvedUserData ? (
+              approvedUserData.map((userData) => (
+                <PostListItem post={userData} />
+              ))
+            ) : (
+              <h1>Not yet!!</h1>
+            )}
+          </section>
+        </main>
+      </Container>
     </div>
   );
 };
