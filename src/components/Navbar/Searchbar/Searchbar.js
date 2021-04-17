ead9910347b3ede8584c1425633d7f6a12046a93
Kris Norful <krisnorful@gmail.com>
2021-04-17 14:30:43 -0500

    retain search term in searchbar after not focused

--- src/components/Navbar/Searchbar/Searchbar.js
+++ src/components/Navbar/Searchbar/Searchbar.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loadSubreddits } from "../../../features/subreddits/subredditsSlice";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import fetch from "cross-fetch";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import SearchIcon from "@material-ui/icons/Search";
+import parse from "autosuggest-highlight/parse";
+import match from "autosuggest-highlight/match";
 
 function sleep(delay = 0) {
   return new Promise((resolve) => {
@@ -33,19 +35,26 @@ const CssTextField = withStyles({
 })(TextField);
 
 export const Searchbar = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("Search...");
   const [open, setOpen] = React.useState(false);
   const [options, setOptions] = React.useState([]);
   const loading = open && options.length === 0;
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
+    let active = true;
     (async () => {
       const response = await fetch(
         `https://www.reddit.com/api/subreddit_autocomplete.json?query=${searchTerm}`
       );
       await sleep(1e3);
       const data = await response.json();
-      setOptions(data.subreddits.map((r) => r));
+      if (active) {
+        setOptions(data.subreddits.map((r) => r));
+      }
+      return () => {
+        active = false;
+      };
     })();
   }, [loading, searchTerm]);
 
@@ -60,10 +69,15 @@ export const Searchbar = () => {
     setSearchTerm(term);
   };
 
+  const handleSearch = () => {
+    dispatch(loadSubreddits(searchTerm));
+    // history.push(`/subreddit/${searchTerm}`);
+  };
+
   return (
     <Autocomplete
-      id="asynchronous-demo"
       style={{ width: 300 }}
+      disableClearable={false}
       open={open}
       onOpen={() => {
         setOpen(true);
@@ -79,10 +93,11 @@ export const Searchbar = () => {
         <CssTextField
           onChange={handleChange}
           {...params}
-          placeholder="Search..."
+          placeholder={searchTerm}
           variant="outlined"
           InputProps={{
             ...params.InputProps,
+            type: "search",
             startAdornment: <SearchIcon />,
             endAdornment: (
               <React.Fragment>
@@ -94,6 +109,23 @@ export const Searchbar = () => {
           }}
         />
       )}
+      renderOption={(option, { inputValue }) => {
+        const matches = match(option.name, inputValue);
+        const parts = parse(option.name, matches);
+        console.log("option", matches);
+        return (
+          <div>
+            {parts.map((part, index) => (
+              <span
+                key={index}
+                style={{ fontWeight: part.highlight ? 700 : 400 }}
+              >
+                {part.text}
+              </span>
+            ))}
+          </div>
+        );
+      }}
     />
   );
 };
