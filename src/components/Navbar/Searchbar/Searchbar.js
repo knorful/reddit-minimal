a59b6251493585185e27f5932d1de664c8d7ba65
Kris Norful <krisnorful@gmail.com>
2021-04-17 13:58:14 -0500

    create auto complete functionality for search bar

--- src/components/Navbar/Searchbar/Searchbar.js
+++ src/components/Navbar/Searchbar/Searchbar.js
@@ -2,63 +2,138 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { loadSubreddits } from "../../../features/subreddits/subredditsSlice";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import fetch from "cross-fetch";
 import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import SearchIcon from "@material-ui/icons/Search";
-import InputAdornment from "@material-ui/core/InputAdornment";
 
-const useStyles = makeStyles((theme) => ({
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+const CssTextField = withStyles({
   root: {
-    display: "flex",
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "50ch",
+    "& .MuiOutlinedInput-root": {
+      "&:hover fieldset": {
+        borderColor: "red",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "red",
+      },
     },
-  },
-  searchInput: {
     backgroundColor: "white",
     borderRadius: "4px",
+    width: "30vw",
     minWidth: "200px",
-    width: "20vw",
   },
-}));
+})(TextField);
 
 export const Searchbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://www.reddit.com/api/subreddit_autocomplete.json?query=${searchTerm}`
+      );
+      await sleep(1e3);
+      const data = await response.json();
+      setOptions(data.subreddits.map((r) => r));
+    })();
+  }, [loading, searchTerm]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
 
   const handleChange = ({ target }) => {
     const term = target.value;
     setSearchTerm(term);
   };
 
-  const handleSearch = () => {
-    dispatch(loadSubreddits(searchTerm));
-    history.push(`/subreddit/${searchTerm}`);
-  };
-
   return (
-    <div className={classes.Searchbar}>
-      <form onSubmit={handleSearch}>
-        <TextField
-          className={classes.searchInput}
+    <Autocomplete
+      id="asynchronous-demo"
+      style={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value) => option.name === value.name}
+      getOptionLabel={(option) => option.name}
+      options={options}
+      loading={loading}
+      renderInput={(params) => (
+        <CssTextField
           onChange={handleChange}
-          aria-label="Search Button"
-          id="outlined-basic"
-          placeholder="Search Communities.."
+          {...params}
+          placeholder="Search..."
           variant="outlined"
-          fullWidth
           InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
+            ...params.InputProps,
+            startAdornment: <SearchIcon />,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+              </React.Fragment>
             ),
           }}
         />
-      </form>
-    </div>
+      )}
+    />
   );
 };
+
+// export const Searchbar = () => {
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const dispatch = useDispatch();
+//   const history = useHistory();
+//   const classes = useStyles();
+
+//   const handleChange = ({ target }) => {
+//     const term = target.value;
+//     setSearchTerm(term);
+//   };
+
+//   const handleSearch = () => {
+//     dispatch(loadSubreddits(searchTerm));
+//     history.push(`/subreddit/${searchTerm}`);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSearch}>
+//         <CssTextField
+//           onChange={handleChange}
+//           aria-label="Search Button"
+//           id="outlined-basic"
+//           placeholder="Search Communities.."
+//           variant="outlined"
+//           fullWidth
+//           InputProps={{
+//             startAdornment: (
+//               <InputAdornment position="start">
+//                 <SearchIcon />
+//               </InputAdornment>
+//             ),
+//           }}
+//         />
+//       </form>
+//       {searchTerm ? <div>searching</div> : null}
+//     </div>
+//   );
+// };
