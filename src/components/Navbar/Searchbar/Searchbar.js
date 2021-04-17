e99862b2d219cc3a10d7c33067bf57fa19d191c2
Kris Norful <krisnorful@gmail.com>
2021-04-17 11:32:27 -0500

    change searchbar to material ui text field

--- src/components/Navbar/Searchbar/Searchbar.js
+++ src/components/Navbar/Searchbar/Searchbar.js
@@ -1,14 +1,33 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import SearchIcon from "@material-ui/icons/Search";
-import classes from "./Searchbar.module.css";
 import { loadSubreddits } from "../../../features/subreddits/subredditsSlice";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import SearchIcon from "@material-ui/icons/Search";
+import InputAdornment from "@material-ui/core/InputAdornment";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "50ch",
+    },
+  },
+  searchInput: {
+    backgroundColor: "white",
+    borderRadius: "4px",
+    minWidth: "200px",
+    width: "20vw",
+  },
+}));
 
 export const Searchbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const dispatch = useDispatch();
   const history = useHistory();
+  const classes = useStyles();
 
   const handleChange = ({ target }) => {
     const term = target.value;
@@ -23,14 +42,22 @@ export const Searchbar = () => {
   return (
     <div className={classes.Searchbar}>
       <form onSubmit={handleSearch}>
-        <input
+        <TextField
+          className={classes.searchInput}
           onChange={handleChange}
-          placeholder="Search Reddits.."
           aria-label="Search Button"
+          id="outlined-basic"
+          placeholder="Search Communities.."
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
         />
-        <button aria-label="search button">
-          <SearchIcon />
-        </button>
       </form>
     </div>
   );
